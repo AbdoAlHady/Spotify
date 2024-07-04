31855a286bf666d3ecbcc532dabbbfbe8612a521
AbdoAlhady <abdo.agiry90@gmail.com>
2024-07-04 17:30:24 +0300

    make splash screen wait 2 seconds and navigate to onboarding screen

---
+++ lib/core/extension/navigate_extension.dart
@@ -0,0 +1,29 @@
+
+import 'package:flutter/material.dart';
+
+extension Navigation on BuildContext {
+
+  // Push Named
+  void pushNamed(String routeName, {Object? arguments}) {
+    Navigator.of(this).pushNamed(routeName, arguments: arguments);
+  }
+
+  // Push Replacement Named
+  void pushReplacementNamed(String routeName, {Object? arguments}) {
+    Navigator.of(this).pushReplacementNamed(routeName, arguments: arguments);
+  }
+
+  Future<dynamic> pushNamedAndRemoveUntil(String routeName,
+      {Object? arguments, required RoutePredicate predicate}) {
+    return Navigator.of(this)
+        .pushNamedAndRemoveUntil(routeName, predicate, arguments: arguments);
+  }
+
+  // Pop
+  void pop() {
+    Navigator.of(this).pop();
+  }
+
+
+ 
+}
