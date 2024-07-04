9f625128db25a64cc5bc56d647a222babd801d9c
AbdoAlhady <abdo.agiry90@gmail.com>
2024-07-04 14:45:44 +0300

    project setup

--- lib/core/configs/theme/app_theme.dart
+++ lib/core/configs/theme/app_theme.dart
@@ -4,41 +4,31 @@ import 'package:spotify_app/core/configs/theme/app_colors.dart';
 class AppTheme {
   static final lightTheme = ThemeData(
     primaryColor: AppColors.primary,
+    fontFamily: 'Satoshi',
     scaffoldBackgroundColor: AppColors.lightBackground,
-    brightness:Brightness.light ,
+    brightness: Brightness.light,
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
-        backgroundColor: AppColors.primary,
-        textStyle: const TextStyle(
-          fontSize: 20,
-          color: Colors.white,
-          fontWeight: FontWeight.bold
-        ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30)
-        )
-      ),
+          backgroundColor: AppColors.primary,
+          textStyle: const TextStyle(
+              fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
     ),
-
   );
 
   static final darkTheme = ThemeData(
     primaryColor: AppColors.primary,
+    fontFamily: 'Satoshi',
     scaffoldBackgroundColor: AppColors.darkBackground,
-    brightness:Brightness.dark ,
+    brightness: Brightness.dark,
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
-        backgroundColor: AppColors.primary,
-        textStyle: const TextStyle(
-          fontSize: 20,
-          color: Colors.white,
-          fontWeight: FontWeight.bold
-        ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30)
-        )
-      ),
+          backgroundColor: AppColors.primary,
+          textStyle: const TextStyle(
+              fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
     ),
-
   );
 }
