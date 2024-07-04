88f41378f3aaef91467e228c4e960496390f5b83
AbdoAlhady <abdo.agiry90@gmail.com>
2024-07-04 14:32:27 +0300

    first commit

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF42C83C);
+  static const lightBackground = Color(0xFFF2F2F2);
+  static const darkBackground = Color(0xFF0D0c0C);
+  static const gery = Color(0xffBEBEBE);
+  static const darkGrey = Color(0xFF343434);
+}
