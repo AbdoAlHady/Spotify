ec3abc5b8f8c692c617f63c5f5a1bc33ddf7b679
AbdoAlhady <abdo.agiry90@gmail.com>
2024-07-06 12:12:31 +0300

    sign in screen ui

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -2,7 +2,6 @@ class AppStrings {
   // intro Screen
   static const String introTitle = 'Enjoy listening to music';
   static const String introSubTitle =
-  
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.';
   static const String getStarted = 'Get Started';
   static const String chooseMode = 'Choose Mode';
@@ -15,11 +14,12 @@ class AppStrings {
   static const String register = 'Register';
   static const String fullName = 'Full Name';
   static const String email = 'Email';
-    static const String enterEmail = 'Enter Email';
+  static const String enterEmail = 'Enter Email';
 
   static const String password = 'Password';
   static const String creatAccount = 'Creat Account';
   static const String haveAnAccount = 'Do you have an account? ';
   static const String signIn = 'Sign In';
-  
+  static const String registerNow = 'Register Now';
+  static const String notMember = 'Not a member? ';
 }
--- lib/features/auth/presentation/screens/sign_in_screen.dart
+++ lib/features/auth/presentation/screens/sign_in_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify_app/core/configs/assets/app_verctors.dart';
+import 'package:spotify_app/core/widgets/basic_app_bar.dart';
 
 import '../widgets/sign_in/sign_in_body.dart';
 
@@ -7,8 +11,15 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SignInBody(),
+    return  Scaffold(
+      appBar: BasicAppBar(
+        title: SvgPicture.asset(
+          AppVerctors.logo,
+          height: 33.h,
+          width: 108.w,
+        ),
+      ),
+      body: const SignInBody(),
     );
   }
 }
