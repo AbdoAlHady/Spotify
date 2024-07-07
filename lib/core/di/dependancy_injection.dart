e440d15645c5fd100493dc161092c013c22211ef
AbdoAlhady <abdo.agiry90@gmail.com>
2024-07-08 01:42:09 +0300

    added sign in use case to dependancy injection

--- lib/core/di/dependancy_injection.dart
+++ lib/core/di/dependancy_injection.dart
@@ -4,6 +4,7 @@ import 'package:spotify_app/features/auth/data/repos/auth_repo_impl.dart';
 import 'package:spotify_app/features/auth/dmoain/repos/auth_repo.dart';
 import 'package:spotify_app/features/auth/dmoain/usecases/signup_usecase.dart';
 
+import '../../features/auth/dmoain/usecases/signin_usecase.dart';
 import '../services/firestore_service.dart';
 
 final getIt = GetIt.instance;
@@ -17,6 +18,10 @@ Future<void> initDepedencies() async {
   getIt.registerSingleton<AuthRepo>(AuthRepoImpl(getIt(), getIt()));
  // SignUp UseCase
   getIt.registerSingleton<SignupUsecase>(SignupUsecase(getIt()));
+  // SignIn UseCase
+  getIt.registerSingleton<SigninUsecase>(SigninUsecase(getIt()));
+
+
 
 
 
