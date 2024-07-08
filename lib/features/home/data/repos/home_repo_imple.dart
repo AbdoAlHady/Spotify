7431c74c3f4942935afd6683b28547684ac6b548
AbdoAlhady <abdo.agiry90@gmail.com>
2024-07-08 18:09:23 +0300

    Added Home Repo Imple For Get News songs

---
+++ lib/features/home/data/repos/home_repo_imple.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:spotify_app/core/error/failure.dart';
+import 'package:spotify_app/core/services/firestore_service.dart';
+import 'package:spotify_app/features/home/domain/entities/song_entity.dart';
+import 'package:spotify_app/features/home/domain/repos/home_repo.dart';
+
+import '../models/song_model.dart';
+
+class HomeRepoImple extends HomeRepo {
+  final FireStoreService _storeService;
+
+  HomeRepoImple(this._storeService);
+  @override
+  Future<Either<Failure, List<SongEntity>>> getNewSongs() async {
+    try {
+    List<SongModel> songs = [];
+    final result = await _storeService.getNewsSongsFromFirestore();
+    for (var element in result.docs) {
+      songs.add(SongModel.fromJson(element.data() as Map<String, dynamic>));
+    }
+    return Right(songs);
+    } catch (e) {
+      return Left(ServerFailure(message: e.toString()));
+    }
+  }
+}
