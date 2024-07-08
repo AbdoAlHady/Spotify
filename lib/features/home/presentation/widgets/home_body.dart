34cf606dc87e3c554fe8acb006800951d283cef7
AbdoAlhady <abdo.agiry90@gmail.com>
2024-07-08 12:51:53 +0300

    refactors name of root screen to home

--- lib/features/home/presentation/widgets/home_body.dart
+++ lib/features/home/presentation/widgets/home_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class RootBody extends StatelessWidget {
-  const RootBody({super.key});
+class HomeBody extends StatelessWidget {
+  const HomeBody({super.key});
 
   @override
   Widget build(BuildContext context) {
