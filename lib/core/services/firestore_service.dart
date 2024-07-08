f22a94b8519999f7afd4fe2ba5c9f1e79c711902
AbdoAlhady <abdo.agiry90@gmail.com>
2024-07-08 17:56:51 +0300

    added method to get songs

--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -10,7 +10,9 @@ class FireStoreService {
 
   // Save User Data to Firestore
   Future<void> saveUserData(
-     { required String userId, required String email, required String fullName}) async {
+      {required String userId,
+      required String email,
+      required String fullName}) async {
     try {
       await _db.collection(FirebaseConstants.users).doc(userId).set({
         'email': email,
@@ -24,5 +26,15 @@ class FireStoreService {
     } catch (e) {
       throw AppStrings.errorMessage;
     }
+  }
+
+  // Get New Songs
+  Future<QuerySnapshot> getNewsSongsFromFirestore() async {
+    final data = await _db
+        .collection(FirebaseConstants.songs)
+        .orderBy('releasedDate', descending: true)
+        .limit(3)
+        .get();
+    return data;
   }
 }
